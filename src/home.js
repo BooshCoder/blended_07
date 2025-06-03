0ceccf61de1149f634b00b3b4d3d773794cffbec
Eugene <mypro4s@icloud.com>
2025-06-03 13:41:47 +0300

    one-four

--- src/home.js
+++ src/home.js
@@ -1,1 +1,67 @@
-//Логіка сторінки Home
+import { fetchCategories } from "./js/products-api"
+import { markupCategories } from "./js/render-function"
+import { fetchProducts } from "./js/products-api";
+import { markupProducts } from "./js/render-function";
+import { fetchProductsByCategory } from "./js/products-api";
+import { refs } from "./js/refs"
+
+
+
+async function renderCategories() {
+    const categories = await fetchCategories();
+    const allCategories = ["All", ...categories];
+    markupCategories(allCategories);
+}
+
+renderCategories();
+
+
+async function renderInitialProducts() {
+    const products = await fetchProducts();
+    markupProducts(products);
+}
+
+renderInitialProducts();
+
+refs.allCategories.addEventListener("click", handleCategoryClick);
+
+async function handleCategoryClick(event) {
+    const btn = event.target.closest(".categories__btn");
+    if (!btn) return;
+    if (btn.textContent.trim() === "All") {
+        const products = await fetchProducts();
+        markupProducts(products);
+    } else {
+        const category = btn.textContent.trim();
+        const products = await fetchProductsByCategory(category);
+        markupProducts(products);
+    }
+    const category = btn.textContent.trim();
+  
+    // Знімаємо стару активну кнопку
+    const activeBtn = refs.allCategories.querySelector(".categories__btn--active");
+    if (activeBtn) activeBtn.classList.remove("categories__btn--active");
+  
+    // Робимо нову кнопку активною
+    btn.classList.add("categories__btn--active");
+  
+    // Запит продуктів
+    let products = [];
+    try {
+      if (category === "All") {
+        products = await fetchProducts();
+      } else {
+        products = await fetchProductsByCategory(category);
+      }
+  
+      if (!products.length) {
+        refs.notFound.classList.add("not-found--visible");
+        refs.allProducts.innerHTML = "";
+      } else {
+        refs.notFound.classList.remove("not-found--visible");
+        markupProducts(products);
+      }
+    } catch (err) {
+      console.error("😿 Не вдалося отримати продукти:", err);
+    }
+  }
--- src/js/products-api.js
+++ src/js/products-api.js
@@ -1,1 +1,24 @@
 // Функції для роботи з бекендом
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://dummyjson.com';
+
+export async function fetchCategories() {
+    const response = await axios.get('/products/category-list');
+    console.log(response.data);
+    return response.data;
+}
+export async function fetchProducts() {
+    const currentPage = 1;
+    const url = `https://dummyjson.com/products?limit=12&skip=${(currentPage - 1) * 12}`;
+    const response = await axios.get(url);
+    console.log(response.data.products);
+    return response.data.products;
+   
+}
+
+export async function fetchProductsByCategory(category) {
+    const response = await axios.get(`/products/category/${category}`);
+    console.log(response.data.products);
+    return response.data.products;
+  }
